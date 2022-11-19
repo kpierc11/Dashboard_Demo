e6887e0865422441438d3243135cf5a3ca7e8713
Emberwind-Studios <89319126+Emberwind-Studios@users.noreply.github.com>
2022-11-18 20:12:26 -0500

    updates to station cards

--- app/src/components/stationCard/StationCard.tsx
+++ app/src/components/stationCard/StationCard.tsx
@@ -4,46 +4,6 @@ import Paper from "@mui/material/Paper";
 import { Station } from "../../interfaces/Stations";
 
 
-class Creature {
-
-  name:string;
-  type:string;
-  health:number;
-  weapon:string;
-
-
-  constructor(name:string, type:string, health:number, weapon:string){
-    this.name = name; 
-    this.type = type;
-    this.health = health;
-    this.weapon = weapon;
-  };
-
-
-  getName(){
-    return this.weapon; 
-  }
-
-
-  getWeapon(){
-    return this.name; 
-  }
-
-  displayCreatureData(){
-    return "name: " + this.name  + " " +  "type:" + " " + this.type +  " " +  "health: " + this.health + " "  + "weapon: " + this.weapon;
-  }
-
-}
-
-
-const creature1:Creature = new Creature("Joe", "Goblin", 100, "sword");
-
-
-console.log(creature1.displayCreatureData());
-
-
-
-
 
 export default function StationCard(props:Station) {
 
--- app/src/pages/StationsPage.tsx
+++ app/src/pages/StationsPage.tsx
@@ -1,12 +1,21 @@
 import { Box } from '@mui/material';
 import * as React from 'react';
+import { findRenderedDOMComponentWithClass } from 'react-dom/test-utils';
 import StationCard from '../components/stationCard/StationCard';
 
 
 export default function StationsPage() {
+
+  let stationCards = [];
+
+  for(let i = 0; i < 100; i++) {
+    stationCards.push(<StationCard key={i} deviceName={''} stationLocation={''} lastReported={''} stationType={''} status={false}></StationCard>);
+  }
+
+
   return (
     <div>
-      <StationCard deviceName={''} stationLocation={''} lastReported={''} stationType={''} status={false} ></StationCard>
+    {stationCards}
     </div>
   );
 }
