a77ccb5ee5bb63aa68cec8276f39b291bff70094
Emberwind-Studios <89319126+Emberwind-Studios@users.noreply.github.com>
2022-11-18 20:55:47 -0500

    removed unused dependencies

--- app/src/pages/StationsPage.tsx
+++ app/src/pages/StationsPage.tsx
@@ -1,21 +1,20 @@
-import { Box } from '@mui/material';
-import * as React from 'react';
-import { findRenderedDOMComponentWithClass } from 'react-dom/test-utils';
-import StationCard from '../components/stationCard/StationCard';
-
+import StationCard from "../components/stationCard/StationCard";
 
 export default function StationsPage() {
-
   let stationCards = [];
 
-  for(let i = 0; i < 100; i++) {
-    stationCards.push(<StationCard key={i} deviceName={''} stationLocation={''} lastReported={''} stationType={''} status={false}></StationCard>);
+  for (let i = 0; i < 100; i++) {
+    stationCards.push(
+      <StationCard
+        key={i}
+        deviceName={""}
+        stationLocation={""}
+        lastReported={""}
+        stationType={""}
+        status={false}
+      ></StationCard>
+    );
   }
 
-
-  return (
-    <div>
-    {stationCards}
-    </div>
-  );
+  return <div>{stationCards}</div>;
 }
