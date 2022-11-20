56bda42ff8837a8c88525852ebaa9f4247915746
Emberwind-Studios <89319126+Emberwind-Studios@users.noreply.github.com>
2022-11-20 18:17:50 -0500

    added new cards to stations page

--- app/src/pages/StationsPage.tsx
+++ app/src/pages/StationsPage.tsx
@@ -24,6 +24,27 @@ export default function StationsPage() {
         stationType={"Kktol"}
         status={false}
       ></StationCard>
+      <StationCard
+        deviceName={"HydroBio e-tracker"}
+        stationLocation={"Piney Flats, TN"}
+        lastReported={"September 23rd 2022 8:56:58 pm"}
+        stationType={"AHPS"}
+        status={false}
+      ></StationCard>
+      <StationCard
+        deviceName={"Test Device"}
+        stationLocation={"Elizabethton, TN"}
+        lastReported={"September 30rd 2022 8:56:58 pm"}
+        stationType={"Kktol"}
+        status={false}
+      ></StationCard>
+      <StationCard
+        deviceName={"Test Device"}
+        stationLocation={"Elizabethton, TN"}
+        lastReported={"September 30rd 2022 8:56:58 pm"}
+        stationType={"Kktol"}
+        status={false}
+      ></StationCard>
     </>
   );
 }
