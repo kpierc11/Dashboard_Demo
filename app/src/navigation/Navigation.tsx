d5522ff23f95c1f4d657ce7508fee3289344e682
Emberwind-Studios <89319126+Emberwind-Studios@users.noreply.github.com>
2022-12-19 18:00:04 -0500

    updates to station cards ui

--- app/src/navigation/Navigation.tsx
+++ app/src/navigation/Navigation.tsx
@@ -1,5 +1,5 @@
 import { createBrowserRouter } from "react-router-dom";
-import StationsPage from "../pages/StationsPage";
+import StationsPage from "../pages/stationsPage/StationsPage";
 
 export const router = createBrowserRouter([
   {
