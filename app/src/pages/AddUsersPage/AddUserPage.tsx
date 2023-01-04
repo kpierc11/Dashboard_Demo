4543d5e793acf02b33c5055591450ad36b22ffd2
Emberwind-Studios <89319126+Emberwind-Studios@users.noreply.github.com>
2023-01-04 01:15:46 -0500

    removed uneeded code

--- app/src/pages/AddUsersPage/AddUserPage.tsx
+++ app/src/pages/AddUsersPage/AddUserPage.tsx
@@ -1,20 +1,10 @@
 import Box from "@mui/material/Box";
-import { Grid, Toolbar } from "@mui/material";
+import { Toolbar } from "@mui/material";
 import { useNavigate } from "react-router-dom";
 import "./addUser.css";
 import AddUserForm, {
   AddUserPhotoForm,
 } from "../../components/addUserForm/AddUserForm";
-import Paper from "@mui/material/Paper";
-import { experimentalStyled as styled } from "@mui/material/styles";
-
-const Item = styled(Paper)(({ theme }) => ({
-  backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
-  ...theme.typography.body2,
-  padding: theme.spacing(2),
-  textAlign: "center",
-  color: theme.palette.text.secondary,
-}));
 
 export default function AddUserPage() {
   const navigate = useNavigate();
